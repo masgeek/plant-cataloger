4266dd2e504ec6c798eeded55916669e6740cec6
Munywele S <barsamms@gmail.com>
2024-07-30 11:20:23 +0300

    feat: added env variables processing

--- config/db.php
+++ config/db.php
@@ -1,14 +1,16 @@
 <?php
 
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+//    'dsn' => 'mysql:host=127.0.0.1;dbname=plant_disease_catalog',
+    'dsn' => getenv('DB_DSN'),
+    'username' => getenv('DB_USERNAME') ?? 'root',
+    'password' => getenv('DB_PASSWORD'),
+    'charset' => getenv('DB_CHARSET') ?? 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
